be2b7a273c900db867a97d419287eaca65908a5d
stellifyv <intanfitriarahmawati311@gmail.com>
2025-10-07 12:01:45 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const tombolSapa = document.getElementById('sapaButton');
+tombolSapa.addEventListener('click', function() {
+  alert('Hai pengelana! terima kasih telah berkunjung di ruang ilmu ini. salam EduChem!');
+});
